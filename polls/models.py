6cb6692035cba4a459518cab4f2093f78925e348
nanami-tomoe <npnp123npnp123@gmail.com>
2024-08-05 23:01:50 +0900

    feat: create model

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+# 모델 생성
+# 모델을 테이블에 써 주기 위한 마이그레이션 이라는걸 만든다.
+# 이 모델에 맞는 테이블을 만든다.
+# 질문: 여름에 놀러간다면 어디에 갈까?
+# 산
+# 강
+# 바다
+# 도심 호캉스
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
